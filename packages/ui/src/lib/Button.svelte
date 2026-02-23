c9ebeb48c38f3c8910a8cd6c52c5a2d5489a2c4e
vijaybaliah <vijaybalu1989@gmail.com>
2026-02-23 12:43:28 +0530

    feat(button): added button styles

--- packages/ui/src/lib/Button.svelte
+++ packages/ui/src/lib/Button.svelte
@@ -7,6 +7,18 @@
   };
 </script>
 
-<button on:click={handleClick}>
+<button on:click={handleClick} class="button">
   {label} ({count})
 </button>
+
+<style lang="scss">
+.button {
+    appearance: none;
+    background: #8937ee;
+    outline: none;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 8px;
+    color: white;
+}
+</style>
---
+++ packages/ui/src/routes/button/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Button from "$lib/Button.svelte";
+</script>
+
+<Button>
+    click me
+</Button>
